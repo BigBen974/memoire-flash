bf0b8b198a26fd719172f26c4f965de8995a600e
BigBen974 <77338703+BigBen974@users.noreply.github.com>
2021-01-13 19:35:59 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ Vue.component('cartes', {
     imageUrl: function (nom) {
       if(nom) return "img/fleches/"+nom.substring(1, 2)+".png"
       else return "img/card.png"
-     
     },
     tryCarte: function (numCarte, event) {
 
@@ -36,13 +35,16 @@ var app = new Vue({
       win: 0,
       bad: 0,
       vie:100,
+      temps:100,
+      time:0,
       onGame: false,
       carte: [], 
       titre:"Réuninap",
       slogan:"",
       essai1: "",
-      inOff:"-off",
+      timeInOff:"-off",
       vieInOff:"-off",
+      stopTimer:0,
     },
     methods:{
 
@@ -57,26 +59,23 @@ var app = new Vue({
         }
         else if (e1.substring(1, 2)!=numCarte.substring(1, 2))
         {
-          
-        
           this.noneCarte();
          
            var cl= setTimeout(this.clearCarte, 400, numCarte, e1);
           this.bad++; 
-          if(this.niveau>=3){ this.vie-=10;}
-          if(this.vie<=0&&this.niveau>=3){
-            this.onGame=false;this.win=0;this.bad=0;this.vie=100;
-            this.carte=[];this.essai1="";this.inOff="-off";this.vieInOff="-off";
+          if(!this.vieInOff){ 
+            this.vie-=10;
+          
+          if(this.vie<=0){
+            this.resetGame(false);
+          }
           }
           
         }else{
-
-          this.noneCarte();
-          
-         
+           this.noneCarte();
           let e = this.essai1;
 
-          if(this.niveau==1||this.niveau==3){
+          if(this.niveau==1||this.niveau==3||this.niveau==5||this.niveau==7){
           var re= setTimeout(this.laisserCarte, 400, numCarte, e);
         }else {
           var re= setTimeout(this.removeCarte, 400, numCarte, e);
@@ -115,11 +114,9 @@ var app = new Vue({
     
     this.win++;
     setTimeout(this.autoCarte, 500);
-
+  
     if(this.carte.length<=0){
-      this.onGame=false;this.win=0;this.niveau++;
-      this.bad=0;this.vie=100;this.essai1="";
-      this.inOff="-off";this.vieInOff="-off";
+      this.resetGame(true);
     }
       },
 
@@ -139,13 +136,24 @@ var app = new Vue({
    this.autoCarte();
    
     this.win++;
-    if(this.win==this.nbrCarte){
-      this.onGame=false;this.win=0;
-      this.carte=[];this.niveau++;this.bad=0;
-      this.vie=100;this.essai1="";
-      this.inOff="-off";this.vieInOff="-off";
-    }
-    
+
+    if(this.win==this.nbrCarte)this.resetGame(true);
+      },
+
+      resetGame: function(n){
+
+        if(n){
+          this.onGame=false;this.win=0;
+          this.carte=[];this.niveau++;this.bad=0;
+          this.vie=100;this.essai1="";
+          this.timeInOff="-off";this.vieInOff="-off";
+          this.stopTimer=0;this.time=0;this.temps=100;
+        }else{       
+               this.onGame=false;this.win=0;this.bad=0;this.vie=100;
+          this.carte=[];this.essai1="";this.timeInOff="-off";
+          this.vieInOff="-off";this.stopTimer=0;this.time=0;this.temps=100;
+        
+        }
       },
       
       autoCarte: function(id){
@@ -202,7 +210,15 @@ var app = new Vue({
       }
       this.carte=this.shuffle(this.carte);
       this.onGame=true;
-      if(this.niveau>=3){this.vieInOff=""; }
+      if(this.niveau==3||this.niveau==4||this.niveau==7||this.niveau>=8){this.vieInOff=""; }
+      if(this.niveau==5||this.niveau==6||this.niveau==7||this.niveau>=8){
+        this.timeInOff="";
+        let timestamp =Date.now();
+        timestamp=Math.floor(timestamp/1000);
+        this.time=timestamp;
+      this.minuteur();
+      }
+
       },
       shuffle: function (array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
@@ -223,11 +239,61 @@ var app = new Vue({
         return array;
       },
       
-      say: function (message) {
-        alert(message)
+      getTimer: function(){
+
+        var seconde=null;
+        var affichage=30;
+        var timestamp =Date.now();
+        timestamp=Math.floor(timestamp/1000);
+        var time = this.time;
+        if(time!=0){
+        var diff=timestamp-time;
+        var decompte=30-diff;
+      
+        
+        seconde=decompte;
+        
+        
+        affichage=seconde;
+
+        this.temps=seconde/0.3;
+        console.log(affichage);
+        console.log(this.temps);
+
+        if(diff>=30){
+          
+          console.log(affichage);
+          this.stopTimer=1;
+          this.minuteur(true);
+          this.resetGame(false);
+  
+        }
+          
       }
+      
+      
+        }	
+      
+      
+      ,
+      minuteur: function(e){
+       if(!e){
+        this.t= setInterval(
+          this.getTimer 
+         
+      , 1000);
+       }
+       console.log(this.stopTimer);
+            if(this.stopTimer==1){
+              clearInterval(this.t); this.stopTimer==0;
+            
+            }
     }
-  })
+    ,}
+  }
+  
+  
+  )
 
  var app = document.getElementById('app');
 
